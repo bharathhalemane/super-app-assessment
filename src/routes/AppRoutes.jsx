498cceeec9729c449f421aeedfb8efc841a3b52e
bharath <bharathhalemane143@gmail.com>
2026-06-24 10:35:02 +0530

    init

---
+++ src/routes/AppRoutes.jsx
@@ -0,0 +1,15 @@
+import {BrowserRouter, Routes, Route} from "react-router-dom"
+
+import Register from "../pages/Register"
+
+const AppRoutes = () => {
+    return (
+        <BrowserRouter>
+            <Routes>
+                <Route path="/" element={<Register/>}/>
+            </Routes>
+        </BrowserRouter>            
+    )
+}
+
+export default AppRoutes
